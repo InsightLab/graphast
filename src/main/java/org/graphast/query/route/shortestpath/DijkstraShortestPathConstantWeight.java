d417a581361c9af701c941d53f520cd584565657
Luis Gustavo Coutinho do Rego <gustavolgcr@gmail.com>
2015-01-22 00:27:47 -0300

    Refactoring the Entry class: creation of TimeEntry and DistanceEntry.

--- src/main/java/org/graphast/query/route/shortestpath/DijkstraShortestPathConstantWeight.java
+++ src/main/java/org/graphast/query/route/shortestpath/DijkstraShortestPathConstantWeight.java
@@ -12,41 +12,51 @@ import org.graphast.model.GraphastNode;
 public class DijkstraShortestPathConstantWeight extends DijkstraShortestPath {
 
 	public DijkstraShortestPathConstantWeight(Graphast graph) {
+		
 		super(graph);
+	
 	}
 	
-	public void expandVertex(GraphastNode target, Entry removed, HashMap<Long, Integer> wasTraversed, 
-			PriorityQueue<Entry> queue, HashMap<Long, RouteEntry> parents){
+	public void expandVertex(GraphastNode target, DistanceEntry removed, HashMap<Long, Integer> wasTraversed, 
+			PriorityQueue<DistanceEntry> queue, HashMap<Long, RouteEntry> parents){
 		
 		Long2IntMap neig = graph.accessNeighborhood(graph.getNode(removed.getId()));
 		
 		for (long vid : neig.keySet()) {
 			//long vid = convertToInt(v);
 			//int arrivalTime = graph.getArrival(removed.getAt(), neig.get(v));
-			int travelTime = removed.getTravelTime() + neig.get(vid);
-			Entry newEntry = new Entry(vid, travelTime, 0, removed.getId());
+			int travelDistance = removed.getDistance() + neig.get(vid);
+			DistanceEntry newDistanceEntry = new DistanceEntry(vid, travelDistance, removed.getId());
 			
 			String label = null;
 			
-			if (!wasTraversed.containsKey(vid)){					
-				queue.offer(newEntry);
-				wasTraversed.put(newEntry.getId(), newEntry.getTravelTime());
+			if (!wasTraversed.containsKey(vid)) {		
+				
+				queue.offer(newDistanceEntry);
+				wasTraversed.put(newDistanceEntry.getId(), newDistanceEntry.getDistance());
 				
 				for(Long outEdges : graph.getOutEdges(removed.getId())) {
+					
 					if ((int) graph.getEdge(outEdges).getToNode() == vid) {
 						label = graph.getEdge(outEdges).getLabel();
 					}
+				
 				}
 				
 				parents.put(vid, new RouteEntry(removed.getId(), neig.get(vid), label));
+				
 			} else {
+				
 				int cost = wasTraversed.get(vid);
+				
 				if (cost != wasRemoved) {
-					if(cost > newEntry.getTravelTime()){
-						queue.remove(newEntry);
-						queue.offer(newEntry);
-						wasTraversed.remove(newEntry.getId());
-						wasTraversed.put(newEntry.getId(), newEntry.getTravelTime());
+					
+					if(cost > newDistanceEntry.getDistance()) {
+						
+						queue.remove(newDistanceEntry);
+						queue.offer(newDistanceEntry);
+						wasTraversed.remove(newDistanceEntry.getId());
+						wasTraversed.put(newDistanceEntry.getId(), newDistanceEntry.getDistance());
 
 						//String label = graph.getEdgeLabel(vid);
 						//parents.put(vid, new RouteEntry(vid, neig.get(vid), label));
--- src/main/java/org/graphast/query/route/shortestpath/LowerBoundEntry.java
+++ src/main/java/org/graphast/query/route/shortestpath/LowerBoundEntry.java
@@ -1,34 +1,46 @@
 package org.graphast.query.route.shortestpath;
 
-
-
-public class LowerBoundEntry extends Entry{
+public class LowerBoundEntry extends TimeEntry{
 	
-	public int lb;
+	public int lowerBound;
 	
-	public LowerBoundEntry(long id, int tt, int at, long parentId, int lb) {
-		super(id,tt,at,parentId);
-		this.lb = lb;
+	public LowerBoundEntry(long id, int travelTime, int arrivalTime, long parentId, int lowerBound) {
+		
+		super(id, travelTime, arrivalTime, parentId);
+		this.lowerBound = lowerBound;
+	
 	}
 	
-	public boolean equals(LowerBoundEntry o){
+	public boolean equals(LowerBoundEntry o) {
+		
 		return super.getId() == o.getId();
+	
 	}
 	
-	public String toString(){
-		return "( ID:"+super.getId()+" TT:"+super.getTravelTime()+" AT:"+super.getArrivalTime()+" LB:"+lb+" )";
+	public String toString() {
+		
+		return "(ID: " + super.getId() + " Travel Time: " + super.getTravelTime() 
+				+ " Arrival Time: " + super.getArrivalTime() + " Lower Bound: " + lowerBound + ")";
+	
 	}
 
 	@Override
 	public int compareTo(Object o) {
-		return new Integer(lb).compareTo(((LowerBoundEntry)o).lb);
+		
+		return new Integer(lowerBound).compareTo(((LowerBoundEntry)o).lowerBound);
+	
 	}
 
-	public int getLb() {
-		return lb;
+	public int getLowerBound() {
+		
+		return lowerBound;
+	
 	}
 
-	public void setLb(int lb) {
-		this.lb = lb;
+	public void setLowerBound(int lowerBound) {
+		
+		this.lowerBound = lowerBound;
+	
 	}
+
 }
