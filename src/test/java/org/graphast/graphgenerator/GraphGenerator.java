b4e572de7eed36ef38ab696d3445085682f55e39
gustavolgcr <gustavolgcr@gmail.com>
2015-01-29 09:03:03 -0300

    Minor refactoring.

--- src/test/java/org/graphast/graphgenerator/GraphGenerator.java
+++ src/test/java/org/graphast/graphgenerator/GraphGenerator.java
@@ -94,36 +94,36 @@ public class GraphGenerator {
  		v = new NodeImpl(6l, 60d, 0d);
  		graph.addNode(v);
 
- 		short[] costs = {1,2,3,4};
+ 		short[] costs = {3,2,3,4};
  		e = new EdgeImpl(0l, 1l, 1, costs);
  		graph.addEdge(e);
 
- 		costs = new short[]{2,4,6,8,10};
+ 		costs = new short[]{4,2,6,8,10};
  		e = new EdgeImpl(0l, 2l, 5, costs);
  		graph.addEdge(e);
 
- 		costs = new short[]{2};
+ 		costs = new short[]{1,2};
  		e = new EdgeImpl(1l, 2l, 3, costs);
  		graph.addEdge(e);
 
- 		costs = new short[]{1,4,7,6,11};
+ 		costs = new short[]{4,4,7,6,11};
  		e = new EdgeImpl(2l, 3l, 3);
  		graph.addEdge(e);
 
- 		costs = new short[]{10};
+ 		costs = new short[]{1,10};
  		e = new EdgeImpl(3l, 4l, 3, costs);
  		graph.addEdge(e);
 
- 		costs = new short[]{12,13};
+ 		costs = new short[]{2,12,13};
  		e = new EdgeImpl(3l, 5l, 4, costs);
  		graph.addEdge(e);
 
- 		costs = new short[]{9,10,11};
+ 		costs = new short[]{3,9,10,11};
  		e = new EdgeImpl(4l, 5l, 2, costs);
  		graph.addEdge(e);
 
- 		costs = new short[]{2,4,6,8,10};
- 		e = new EdgeImpl(5l, 6l, 1);
+ 		costs = new short[]{5,2,4,6,8,15};
+ 		e = new EdgeImpl(5l, 6l, 1, costs);
  		graph.addEdge(e);
 
  		System.out.println(graph.getCosts());
