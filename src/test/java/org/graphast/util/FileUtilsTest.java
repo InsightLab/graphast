a17849a9de7137604ab6bae5256ddf4062033366
Regis Pires Magalhaes <regispiresmag@gmail.com>
2015-01-31 10:21:05 -0300

    created enum CompressionType. Now a compression type can be set to a graph. We have 2 compression types: NO_COMPRESSION and GZIP_COMPRESSION

--- src/test/java/org/graphast/util/FileUtilsTest.java
+++ src/test/java/org/graphast/util/FileUtilsTest.java
@@ -11,13 +11,13 @@ import org.junit.Test;
 public class FileUtilsTest {
 
 	protected int blockSize = 4096;
-	
+
 	@Test
 	public void read() throws Exception{
 		String file = this.getClass().getResource("/fileTest.txt").getFile();
 		assertEquals("blablabla\nabc\n", FileUtils.read(file));
 	}
-	
+
 	@Test
 	public void Long2ShortMapTest() throws Exception {
 		Long2IntMap testMap = new Long2IntOpenHashMap();
@@ -27,14 +27,14 @@ public class FileUtilsTest {
 
 		FileUtils.saveLong2IntMap(Configuration.USER_HOME + "/graphast/test/test-file-nocompression", testMap, blockSize, CompressionType.NO_COMPRESSION);
 		Long2IntMap loadedMap = FileUtils.loadLong2IntMap(Configuration.USER_HOME + "/graphast/test/test-file-nocompression", blockSize, CompressionType.NO_COMPRESSION);
-		
+
 		assertEquals(100,loadedMap.get(10000L));
 		assertEquals(200,loadedMap.get(20000L));
 		assertEquals(300,loadedMap.get(30000L));
-		
+
 		FileUtils.saveLong2IntMap(Configuration.USER_HOME + "/graphast/test/test-file-gzipcompression", testMap, blockSize, CompressionType.GZIP_COMPRESSION);
 		loadedMap = FileUtils.loadLong2IntMap(Configuration.USER_HOME + "/graphast/test/test-file-gzipcompression", blockSize, CompressionType.GZIP_COMPRESSION);
-		
+
 		assertEquals(100,loadedMap.get(10000L));
 		assertEquals(200,loadedMap.get(20000L));
 		assertEquals(300,loadedMap.get(30000L));
