92ab8fb5a085f2433248d1e5601fac8f6da1019d
matheus <matheus@barrio.com.br>
2015-02-04 11:20:27 -0300

    modification on Bounds of KNN. need of cost modification

--- src/main/java/org/graphast/query/model/BoundsSearch.java
+++ src/main/java/org/graphast/query/model/BoundsSearch.java
@@ -1,27 +1,11 @@
 package org.graphast.query.model;
 
-<<<<<<< HEAD
 import it.unimi.dsi.fastutil.longs.Long2IntMap;
-=======
-import it.unimi.dsi.fastutil.longs.Long2ShortMap;
->>>>>>> modification on Bounds of KNN. need of cost modification
 
 
 
 public interface BoundsSearch {
-<<<<<<< HEAD
 	public Long2IntMap getBounds();
 	
 	public void setBounds(Long2IntMap bounds) ;
 }
-=======
-	public Long2ShortMap getBounds();
-	
-<<<<<<< HEAD:src/main/java/org/graphast/query/model/BoundsSearch.java
-	public void setBounds(HashMap<Long, Integer> bounds) ;
-}
-=======
-	public void setBounds(Long2ShortMap bounds) ;
-}
->>>>>>> modification on Bounds of KNN. need of cost modification:src/main/java/org/graphast/query/knn/model/BoundsSearch.java
->>>>>>> modification on Bounds of KNN. need of cost modification
