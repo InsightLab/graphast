78379422e76ec9ec41a625dd5869a4fd68e6dc01
matheus <matheus@barrio.com.br>
2015-03-12 16:15:07 -0300

    KNNTC working, tests done

--- src/test/java/org/graphast/knn/KNNTCSearchTest.java
+++ src/test/java/org/graphast/knn/KNNTCSearchTest.java
@@ -2,21 +2,20 @@ package org.graphast.knn;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
+import org.graphast.config.Configuration;
 import org.graphast.enums.GraphBoundsType;
 import org.graphast.graphgenerator.GraphGenerator;
 import org.graphast.model.GraphBounds;
-import org.graphast.query.knn.BoundsKNN;
 import org.graphast.query.knn.BoundsKNNTC;
 import org.graphast.query.knn.KNNTCSearch;
 import org.graphast.query.knn.NearestNeighbor;
 import org.graphast.util.DateUtils;
 import org.graphast.util.FileUtils;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
@@ -39,34 +38,21 @@ public abstract class KNNTCSearchTest {
 	}
 	
 	@Test
-	public void search() throws Exception{
+	public void search() throws ParseException{
 		Date date = DateUtils.parseDate(0, 550, 0);
     	
-    	List<NearestNeighbor> nn = knn.search(graphPoI.getVertexByOId(1), date, 2);
+    	List<NearestNeighbor> nn = knn.search(graphPoI.getNode(7), date, 2);
     			
-    	assertEquals(convertToInt(graphPoI.getVertexByOId(2).getId()), (int) nn.get(0).getId());
-		assertEquals(900000, nn.get(0).getDistance());
-		assertEquals(convertToInt(graphPoI.getVertexByOId(5).getId()), (int) nn.get(1).getId());
-		assertEquals(3120000, nn.get(1).getDistance());
+    	assertEquals((long)graphPoI.getNode(4).getId(), nn.get(0).getId());
+		assertEquals(190, nn.get(0).getDistance());
+		assertEquals((long)graphPoI.getNode(9).getId(), nn.get(1).getId());
+		assertEquals(260, nn.get(1).getDistance());
 				
-	}
-	
-	@Autowired
-	@Qualifier("testDir")
-	public void setTestDir(String dir){
-		testDir = dir;
-	}
-	
-	@After
-	public void tearDown(){
-		graphPoI.shutdown();
-		lbgAdapter.shutdown();
-		ubgAdapter.shutdown();
 	}
 	
 	@AfterClass
 	public static void shutdown() throws IOException{
-		FileUtils.deleteDirectory(new File(testDir));
+		FileUtils.deleteDir(Configuration.USER_HOME + "/graphast/test/examplePoI");
 	}
-	
-}
+}	
+
