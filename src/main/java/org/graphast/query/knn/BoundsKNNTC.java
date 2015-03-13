80f798010a76806a995fb58c726cd52f031caea7
matheus <matheus@barrio.com.br>
2015-03-13 17:54:34 -0300

    not working, problems on refactoring BoundsKNN and BoundsKNNTC

--- src/main/java/org/graphast/query/knn/BoundsKNNTC.java
+++ src/main/java/org/graphast/query/knn/BoundsKNNTC.java
@@ -1,21 +1,26 @@
 package org.graphast.query.knn;
 
 import it.unimi.dsi.fastutil.BigArrays;
+import it.unimi.dsi.fastutil.objects.ObjectCollection;
 
 import org.graphast.enums.GraphBoundsType;
 import org.graphast.model.GraphBounds;
 import org.graphast.model.Node;
-import org.graphast.query.model.AbstractBoundsSearch;
+import org.graphast.query.model.AbstractBoundsSearchPoI;
+import org.graphast.query.model.Bound;
 import org.graphast.query.route.shortestpath.dijkstra.DijkstraGeneric;
 
-public class BoundsKNNTC extends AbstractBoundsSearch{
+public class BoundsKNNTC extends AbstractBoundsSearchPoI{
 	
 	public BoundsKNNTC(GraphBounds graph, GraphBoundsType type){
 			DijkstraGeneric d = new DijkstraGeneric(graph);
 			for(int i = 0; i < graph.getNumberOfNodes(); i++){
 				long position = i*Node.NODE_BLOCKSIZE;
 				long vid = BigArrays.index(graph.getNodes().getInt(position), graph.getNodes().getInt(position + 1));
-					bounds.put(vid,  d.shortestTS(vid, type).getCost());
+				Bound b = new Bound(vid,  d.shortestTS(vid, type).getCost());
+				ObjectCollection<Bound> oc = null;
+				oc.add(b);
+				bounds.put(vid, oc);	
 			}
 	}		
 }
