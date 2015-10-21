0265a8de0eedc82104a07bda278807c0faf4fbcc
Luís Gustavo Coutinho do Rêgo <gustavolgcr@gmail.com>
2015-10-21 12:30:17 -0300

    Path refactoring. A* still with some bugs.

--- core/src/test/java/org/graphast/query/route/shortestpath/AbstractShortestPathTest.java
+++ core/src/test/java/org/graphast/query/route/shortestpath/AbstractShortestPathTest.java
@@ -3,6 +3,7 @@ package org.graphast.query.route.shortestpath;
 import static org.junit.Assert.assertEquals;
 
 import org.graphast.config.Configuration;
+import org.graphast.geometry.Point;
 import org.graphast.graphgenerator.GraphGenerator;
 import org.graphast.model.Graph;
 import org.graphast.query.route.shortestpath.model.Path;
@@ -19,13 +20,13 @@ public abstract class AbstractShortestPathTest {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	protected static Graph graphMonaco;
-	protected static Graph graphWashington;
+//	protected static Graph graphWashington;
 	protected static Graph graphExample;
 	protected static Graph graphExample2;
 	
 	
 	protected static AbstractShortestPathService serviceMonaco;
-	protected static AbstractShortestPathService serviceWashington;
+//	protected static AbstractShortestPathService serviceWashington;
 	protected static AbstractShortestPathService serviceExample;
 	protected static AbstractShortestPathService serviceExample2;
 	
@@ -34,7 +35,7 @@ public abstract class AbstractShortestPathTest {
 	@BeforeClass
 	public static void setup() {
 		graphMonaco = new GraphGenerator().generateMonaco();
-		graphWashington = new GraphGenerator().generateWashington();
+//		graphWashington = new GraphGenerator().generateWashington();
 		graphExample = new GraphGenerator().generateExample();
 		graphExample2 = new GraphGenerator().generateExample2();
 	}
@@ -53,9 +54,14 @@ public abstract class AbstractShortestPathTest {
 
 		logger.debug(shortestPath.toString());
 		logger.debug("Execution Time of shortestPathMonacoTest(): {}ms", sw.getTime());
-		logger.debug("Path Cost: {}", shortestPath.getPathCost());
-
-		assertEquals(228910, shortestPath.getPathCost(), 0);
+		logger.debug("Path Total Distance: {}", shortestPath.getTotalDistance());
+		logger.debug("Path Total Cost: {}", shortestPath.getTotalCost());
+		
+		for(Point point : shortestPath.getGeometry()) {
+			System.out.println("(" + point.getLatitude() + "," + point.getLongitude()+")");
+		}
+		
+		assertEquals(228910, shortestPath.getTotalCost(), 0);
 		
 	}
 
@@ -73,31 +79,41 @@ public abstract class AbstractShortestPathTest {
 
 		logger.debug(shortestPath.toString());
 		logger.debug("Execution Time of shortestPathMonacoTest2(): {}ms", sw.getTime());
-		logger.debug("Path Cost: {}", shortestPath.getPathCost());
+		logger.debug("Path Total Distance: {}", shortestPath.getTotalDistance());
+		logger.debug("Path Total Cost: {}", shortestPath.getTotalCost());
 
-		assertEquals(1136643.0, shortestPath.getPathCost(), 0);
+		for(Point point : shortestPath.getGeometry()) {
+			System.out.println("(" + point.getLatitude() + "," + point.getLongitude()+")");
+		}
+		
+		assertEquals(1136643.0, shortestPath.getTotalCost(), 0);
 
 	}
 	
-	@Test
-	public void shortestPathWashingtonTest() {
-		
-		Long source = graphWashington.getNodeId(47.715529934664,-122.28728992389046);
-		Long target = graphWashington.getNodeId(47.528278027361885,-122.36992412887344);
-
-		StopWatch sw = new StopWatch();
-
-		sw.start();
-		Path shortestPath = serviceWashington.shortestPath(source, target);
-		sw.stop();
-
-		logger.debug(shortestPath.toString());
-		logger.debug("Execution Time of shortestPathWashintonTest(): {}ms", sw.getTime());
-		logger.debug("Path Cost: {}", shortestPath.getPathCost());
-
-//		assertEquals(228910, shortestPath.getPathCost(), 0);
-		
-	}
+//	@Test
+//	public void shortestPathWashingtonTest() {
+//		
+//		Long source = graphWashington.getNodeId(47.715529934664,-122.28728992389046);
+//		Long target = graphWashington.getNodeId(47.528278027361885,-122.36992412887344);
+//
+//		StopWatch sw = new StopWatch();
+//
+//		sw.start();
+//		Path shortestPath = serviceWashington.shortestPath(source, target);
+//		sw.stop();
+//
+//		logger.debug(shortestPath.toString());
+//		logger.debug("Execution Time of shortestPathWashintonTest(): {}ms", sw.getTime());
+//		logger.debug("Path Total Distance: {}", shortestPath.getTotalDistance());
+//		logger.debug("Path Total Cost: {}", shortestPath.getTotalCost());
+//
+//		for(Point point : shortestPath.getGeometry()) {
+//			System.out.println("(" + point.getLatitude() + "," + point.getLongitude()+")");
+//		}
+//
+////		assertEquals(228910, shortestPath.getPathCost(), 0);
+//		
+//	}
 
 	@Test
 	public void shortestPathExampleTest() {
@@ -113,9 +129,14 @@ public abstract class AbstractShortestPathTest {
 
 		logger.debug(shortestPath.toString());
 		logger.debug("Execution Time of shortestPathExampleTest(): {}ms", sw.getTime());
-		logger.debug("Path Cost: {}", shortestPath.getPathCost());
+		logger.debug("Path Total Distance: {}", shortestPath.getTotalDistance());
+		logger.debug("Path Total Cost: {}", shortestPath.getTotalCost());
 
-		assertEquals(8100, shortestPath.getPathCost(), 0);
+		for(Point point : shortestPath.getGeometry()) {
+			System.out.println("(" + point.getLatitude() + "," + point.getLongitude()+")");
+		}
+
+		assertEquals(8100, shortestPath.getTotalCost(), 0);
 
 	}
 	
@@ -155,9 +176,14 @@ public abstract class AbstractShortestPathTest {
 
 		logger.debug(shortestPath.toString());
 		logger.debug("Execution Time of shortestPathMonacoTest3(): {}ms", sw.getTime());
-		logger.debug("Path Cost: {}", shortestPath.getPathCost());
+		logger.debug("Path Total Distance: {}", shortestPath.getTotalDistance());
+		logger.debug("Path Total Cost: {}", shortestPath.getTotalCost());
 
-		assertEquals(3610712.0, shortestPath.getPathCost(), 0);
+		for(Point point : shortestPath.getGeometry()) {
+			System.out.println("(" + point.getLatitude() + "," + point.getLongitude()+")");
+		}
+
+		assertEquals(3610712.0, shortestPath.getTotalCost(), 0);
 
 	}
 	
@@ -177,13 +203,15 @@ public abstract class AbstractShortestPathTest {
 
 		logger.debug(shortestPath.toString());
 		logger.debug("Execution Time of shortestPathMonacoTest3(): {}ms", sw.getTime());
-		logger.debug("Path Cost: {}", shortestPath.getPathCost());
+		logger.debug("Path Total Distance: {}", shortestPath.getTotalDistance());
+		logger.debug("Path Total Cost: {}", shortestPath.getTotalCost());
 
-		assertEquals(3610712.0, shortestPath.getPathCost(), 0);
+		for(Point point : shortestPath.getGeometry()) {
+			System.out.println("(" + point.getLatitude() + "," + point.getLongitude()+")");
+		}
+
+		assertEquals(3610712.0, shortestPath.getTotalCost(), 0);
 	}
-	
-	
-
 	
 	@AfterClass
 	public static void tearDown() {
