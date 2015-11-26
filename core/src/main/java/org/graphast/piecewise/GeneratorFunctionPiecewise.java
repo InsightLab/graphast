26695f95360ea7b929b03f90394c5cc3e4cbef22
Mirla Braga <mirlabraga@gmail.com>
2015-11-26 14:36:53 -0200

    Criada primeira função após o uso do loess

--- core/src/main/java/org/graphast/piecewise/GeneratorFunctionPiecewise.java
+++ core/src/main/java/org/graphast/piecewise/GeneratorFunctionPiecewise.java
@@ -1,9 +1,5 @@
 package org.graphast.piecewise;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-
 import rcaller.RCaller;
 import rcaller.RCode;
 
@@ -11,47 +7,77 @@ public class GeneratorFunctionPiecewise {
 	
 	private static final String RSCRIPT = "/usr/bin/Rscript";
 
-	public double getValue(Date timeDay) {
+	public double getValue(double timestamp) throws PiecewiseException {
 		
 		IGeneratorFunctionPiecewise generatorFunctionPiecewise = new GeneratorFunctionPiecewiseDatabase();
-		
-		double[][] data = null;
-		try {
-			data = generatorFunctionPiecewise.getData();
-		} catch (PiecewiseException e) {
-			e.printStackTrace();
-		}
-		
-//		List<Object> executeLossFuntion = executeLossFuntion(data);
-		
-		return 0;
+		double[][] coleta = generatorFunctionPiecewise.getData();
+		return getFuntionEdge(coleta, timestamp);
 	}
 
-	private List<Object> executeLossFuntion(double[][] data ) throws PiecewiseException {
+	private double getFuntionEdge(double[][] data, double timestamp) throws PiecewiseException {
 		
 		RCaller caller = new RCaller();
 		RCode code = caller.getRCode();
 		caller.setRscriptExecutable(RSCRIPT);
 		
-		code.addDoubleMatrix("dados.frame", data);
-		code.addRCode("dados.loess <- loess(y ~ x)");
-		code.addRCode("xl <- seq(min(x), max(x), (max(x) - min(x))/1000)");
+		code.addDoubleMatrix("matrix", data);
+		code.addRCode("dados.frame <- data.frame(matrix)");
+		code.addRCode("dados.loess <- loess(dados.frame)");
+		code.addRCode("xl <- with(dados.loess, seq(min(x), max(x), (max(x) - min(x))/1000))");
 		code.addRCode("y.predict <- predict(dados.loess, xl)");
 		code.addRCode("infl <- c(FALSE, diff(diff(y.predict)>0)!=0)");
-		code.addRCode("objMin <- cbind(xl, y.predict)[xl == min(xl),]");
-		code.addRCode("obj <- cbind(xl, y.predict)[infl,]");
-		code.addRCode("objMax <- cbind(xl, y.predict)[xl == max(xl),]");
-		code.addRCode("allObjects<-list(resultMin=objMin, resultMax=objMax, resultObj=obj)");
-
+		code.addRCode("pontoInicial <- cbind(xl, y.predict)[xl == min(xl),]");
+		code.addRCode("pontosInflexao <- cbind(xl, y.predict)[infl,]");
+		code.addRCode("pontoFinal <- cbind(xl, y.predict)[xl == max(xl),]");
+		code.addRCode("allObjects <- list(pontoInicial=pontoInicial, pontoFinal=pontoFinal, pontosInflexao=pontosInflexao)");
+		
 		caller.setRCode(code);
 		caller.runAndReturnResult("allObjects");
 
-		try {
-			System.out.println(caller.getParser().getXMLFileAsString());
-		} catch (IOException e) {
-			throw new PiecewiseException("[ERRO] Um erro quando estava sendo executada a função LOESS do R.");
+		double[] pontoInicial = caller.getParser().getAsDoubleArray("pontoInicial");
+		double[] pontosInflexao = caller.getParser().getAsDoubleArray("pontosInflexao");
+		
+		PontoGeometrico pontoInicialGeo = new PontoGeometrico(pontoInicial[0], pontoInicial[1]);
+		PontoGeometrico pontoFinalGeo = new PontoGeometrico(pontosInflexao[0], pontosInflexao[pontosInflexao.length/2]);
+		
+		double coeficienteAngular = getCoeficienteAngular(pontoInicialGeo, pontoFinalGeo);
+		double coeficienteLinear = getCoeficienteLinear(pontoInicialGeo, coeficienteAngular);
+		double y = coeficienteAngular *  timestamp + coeficienteLinear;
+		
+		return y;
+	}
+	
+	private double getCoeficienteLinear(PontoGeometrico pontoInicialGeo, double coeficienteAngular) {
+		double gama = pontoInicialGeo.getY() - coeficienteAngular * pontoInicialGeo.getX();  
+		return gama;
+	}
+
+	private double getCoeficienteAngular(PontoGeometrico pontoInicialGeo, PontoGeometrico pontoFinalGeo) {
+		double alfa = (pontoFinalGeo.getY() - pontoInicialGeo.getY()) / (pontoFinalGeo.getX() - pontoInicialGeo.getX());
+		return alfa;
+	}
+
+	class PontoGeometrico {
+		
+		double x;
+		double y;
+		
+		public PontoGeometrico(double x, double y) {
+			this.x = x;
+			this.y= y;
 		}
 		
-		return null;
+		public double getX() {
+			return x;
+		}
+		public void setX(double x) {
+			this.x = x;
+		}
+		public double getY() {
+			return y;
+		}
+		public void setY(double y) {
+			this.y = y;
+		}
 	}
 }
--- core/src/test/java/org/graphast/piecewise/GeneratorFunctionPiecewiseDatabaseTest.java
+++ core/src/test/java/org/graphast/piecewise/GeneratorFunctionPiecewiseDatabaseTest.java
@@ -8,7 +8,12 @@ import org.graphast.util.ConnectionJDBC;
 import org.junit.Assert;
 import org.junit.Test;
 
+import rcaller.RCaller;
+import rcaller.RCode;
+
 public class GeneratorFunctionPiecewiseDatabaseTest {
+	
+	private static final String RSCRIPT = "/usr/bin/Rscript";
 
 	@Test
 	public void getConnectionTest() throws ClassNotFoundException, SQLException, IOException {
@@ -25,5 +30,54 @@ public class GeneratorFunctionPiecewiseDatabaseTest {
 		int length = data.length;
 		
 		Assert.assertEquals(28, length);
+	}
+	
+	@Test
+	public void verifyPointsTest() throws IOException, PiecewiseException {
+		
+		RCaller caller = new RCaller();
+		RCode code = caller.getRCode();
+		caller.setRscriptExecutable(RSCRIPT);
+
+		IGeneratorFunctionPiecewise generatorFunctionPiecewise = new GeneratorFunctionPiecewiseDatabase();
+		double[][] data = generatorFunctionPiecewise.getData();
+		
+		code.addDoubleMatrix("matrix", data);
+		code.addRCode("dados.frame <- data.frame(matrix)");
+		code.addRCode("dados.loess <- loess(dados.frame)");
+		code.addRCode("xl <- with(dados.loess, seq(min(x), max(x), (max(x) - min(x))/1000))");
+		code.addRCode("y.predict <- predict(dados.loess, xl)");
+		code.addRCode("infl <- c(FALSE, diff(diff(y.predict)>0)!=0)");
+		code.addRCode("pontoInicial <- cbind(xl, y.predict)[xl == min(xl),]");
+		code.addRCode("pontosInflexao <- cbind(xl, y.predict)[infl,]");
+		code.addRCode("pontoFinal <- cbind(xl, y.predict)[xl == max(xl),]");
+		code.addRCode("allObjects <- list(pontoInicial=pontoInicial, pontoFinal=pontoFinal, pontosInflexao=pontosInflexao)");
+		
+		caller.setRCode(code);
+		caller.runAndReturnResult("allObjects");
+
+		Assert.assertNotNull(caller.getParser().getXMLFileAsString());
+		System.out.println(caller.getParser().getXMLFileAsString().toString());
+		
+		double[] pontoInicial = caller.getParser().getAsDoubleArray("pontoInicial");
+		Assert.assertEquals(Double.valueOf("1201977368000"), Double.valueOf(pontoInicial[0]));
+		Assert.assertEquals(Double.valueOf("295522763826171"), Double.valueOf(pontoInicial[1]));
+		
+		double[] pontosInflexao = caller.getParser().getAsDoubleArray("pontosInflexao");
+		int length = pontosInflexao.length;
+		Assert.assertEquals(6, length);
+		
+		Assert.assertEquals(Double.valueOf("1202063763696"), Double.valueOf(pontosInflexao[0]));
+		Assert.assertEquals(Double.valueOf("55328186.6718641"), Double.valueOf(pontosInflexao[length/2]));
+	}
+	
+	@Test
+	public void generationFunctionEdgeTest() throws NumberFormatException, PiecewiseException {
+		
+		GeneratorFunctionPiecewise generatorFunctionPiecewise = new GeneratorFunctionPiecewise();
+		double value = generatorFunctionPiecewise.getValue(Double.valueOf("1201977368000"));
+		
+		System.out.println(value);
+		Assert.assertNotNull(value);
 	}
 }
