532458c4e101c75966ee10d7494cab93cc4035f1
Mirla Braga <mirlabraga@gmail.com>
2016-03-14 12:58:52 -0300

    Insert into para piecewise table.

--- core/src/main/java/org/graphast/query/postgis/QueryPostgis.java
+++ core/src/main/java/org/graphast/query/postgis/QueryPostgis.java
@@ -4,6 +4,6 @@ public interface QueryPostgis {
 
 	String QUERY_ALL_DATE_TIME_WITH_DURATE = "SELECT date_time, duracao FROM tester;";
 	String QUERY_CLOSER_LINESTRING = null;
-	String INSERT_PIECEWISE = "INSERT INTO public.tester(edgeId, timeDay, totalTime) VALUES (?, ?,?);";
+	String INSERT_PIECEWISE = "INSERT INTO public.piecewise(edgeId, timeDay, totalTime) VALUES (?, ?,?);";
 
 }
