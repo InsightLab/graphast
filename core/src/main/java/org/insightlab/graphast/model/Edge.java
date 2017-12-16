8cf0fcd953311ceb809837b3bf6dd4b69d530cf7
Filipe Lobo <filipe.lobo.ssa@gmail.com>
2017-12-16 20:52:33 -0300

    Javadoc of classes in model package created. Texts still need validation and semantic/syntactic verification. #48

--- core/src/main/java/org/insightlab/graphast/model/Edge.java
+++ core/src/main/java/org/insightlab/graphast/model/Edge.java
@@ -1,5 +1,9 @@
 package org.insightlab.graphast.model;
 
+/**
+ * The Edge class. It represents the model of a graph edge.
+ * It extends the GraphObject abstract class.
+ */
 public class Edge extends GraphObject {
 	
 	private long fromNode;
@@ -7,57 +11,141 @@ public class Edge extends GraphObject {
 	private double cost;
 	private boolean bidirectional;
 	
+	/**
+	 * Instantiates a new edge.
+	 *
+	 * @param from the id from the outgoing node.
+	 * @param to the id from the incoming node.
+	 */
 	public Edge(long from, long to) {
 		this(from, to, 1);
 	}
 	
+	/**
+	 * Instantiates a new edge.
+	 *
+	 * @param from the id from the outgoing node.
+	 * @param to the id from the incoming node.
+	 * @param cost the cost value of the edge.
+	 */
 	public Edge(long from, long to, double cost) {
 		this.fromNode = from;
 		this.toNode= to;
 		this.cost = cost;
 	}
 	
+	/**
+	 * Instantiates a new edge.
+	 *
+	 * @param from the id from the outgoing node.
+	 * @param to the id from the incoming node.
+	 * @param bidirectional true, if the edge is bidirectional. false, otherwise.
+	 */
 	public Edge(long from, long to, boolean bidirectional) {
 		this(from, to, 1, bidirectional);
 	}
 	
+	/**
+	 * Instantiates a new edge.
+	 *
+	 * @param from the id from the outgoing node.
+	 * @param to the id from the incoming node.
+	 * @param cost the cost value of the edge.
+	 * @param bidirectional true, if the edge is bidirectional. false, otherwise.
+	 */
 	public Edge(long from, long to, double cost, boolean bidirectional) {
 		this(from, to, cost);
 		this.bidirectional = bidirectional;
 	}
 	
+	/**
+	 * Gets the id of the node in which the edge is outing from. 
+	 *
+	 * @return the id from the outing node of the edge.
+	 */
 	public long getFromNodeId() {
 		return fromNode;
 	}
 	
+	/**
+	 * Gets the id of the node in which the edge is going into.
+	 *
+	 * @return the id from the incoming node of the edge.
+	 */
 	public long getToNodeId() {
 		return toNode;
 	}
 	
+	/**
+	 * Gets the cost value of the edge.
+	 *
+	 * @return the cost of the edge (double).
+	 */
 	public double getCost() {
 		return cost;
 	}
 	
+	/**
+	 * Sets the id of the node in which the edge is outing from.
+	 *
+	 * @param fromNode the new id from the outing node of the edge.
+	 */
 	public void setFromNodeId(long fromNode) {
 		this.fromNode = fromNode;
 	}
 	
+	/**
+	 * sets the id of the node in which the edge is going into.
+	 *
+	 * @param toNode the new id from the incoming node of the edge.
+	 */
 	public void setToNodeId(long toNode) {
 		this.toNode = toNode;
 	}
 	
+	/**
+	 * Sets the cost value of the edge.
+	 *
+	 * @param cost the new cost of the edge.
+	 */
 	public void setCost(double cost) {
 		this.cost = cost;
 	}
 	
+	/**
+	 * Checks if the edge is bidirectional, e.g. the 
+	 * edge points both in and outward its two nodes.
+	 *
+	 * @return true, if the edge is bidirectional.
+	 */
 	public boolean isBidirectional() {
 		return bidirectional;
 	}
 	
+	/**
+	 * Gets the id from a node adjacent to this edge in the graph, give a node id.
+	 * The given id is compared to the value of the incoming node id from this edge.
+	 * If the two id values match, the id from the outgoing node of this edge is returned.
+	 * Otherwise, the id from the incoming node is returned.
+	 *
+	 * @param id the id from the node from which we wish to discover its adjacent.
+	 * @return the id from the adjacent node, based on the given id.
+	 */
 	public long getAdjacent(long id) {
 		return id == toNode ? fromNode : toNode;
 	}
 	
+	/**
+	 * Equals method. It receives an object and checks if it is an Edge object
+	 * and if it has the same incoming node id, the same outcoming node id and
+	 * the same cost value. It uses the string representation of the edges to 
+	 * calculate this equality.
+	 *
+	 * @param obj the object to check equality.
+	 * @return true, if the received object is an Edge object and it has the same
+	 * values for its attributes than the instance calling this function. false, 
+	 * otherwise.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (!(obj instanceof Edge)) return false;
@@ -66,11 +154,22 @@ public class Edge extends GraphObject {
 				.equals(other.getFromNodeId()+"|"+other.getToNodeId()+"|"+other.getCost());
 	}
 	
+	/**
+	 * Hash code function of the object (overridden).
+	 *
+	 * @return the 32-bit signed integer hash value of the Edge's toString 
+	 * representation's instance id.
+	 */
 	@Override
 	public int hashCode() {
 		return (fromNode + "|" + toNode + "|" + cost).hashCode();
 	}
 	
+	/**
+	 * To string function (overridden).
+	 *
+	 * @return the string representation of the edge values.
+	 */
 	@Override
 	public String toString() {
 		return fromNode + "|" + toNode + "|" + cost;
--- core/src/main/java/org/insightlab/graphast/model/GraphObject.java
+++ core/src/main/java/org/insightlab/graphast/model/GraphObject.java
@@ -2,15 +2,36 @@ package org.insightlab.graphast.model;
 
 import java.util.HashMap;
 
+/**
+ * The abstract class GraphObject. The basic models of graph elements 
+ * in the project extend from this class.
+ */
 public abstract class GraphObject {
 	
 private HashMap<String, Object> data = null;
 	
+	/**
+	 * Put data function. Given a key and a value, represented by an object,
+	 * it adds a new element into its data HashMap. If its HashMap is null,
+	 * this function instantiates it and then adds the new element.
+	 *
+	 * @param key key with which the specified value is to be associated.
+	 * @param value value to be associated with the specified key.
+	 */
 	public void putData(String key, Object value) {
 		if(data == null) data = new HashMap<>();
 		data.put(key, value);
 	}
 	
+	/**
+	 * Gets data function. Given a key, it retrieves an object from the data
+	 * HashMap and returns it. If the data HashMap does not contain the given key,
+	 * null is returned.
+	 *
+	 * @param key the key whose associate value is to be returned.
+	 * @return the value to which the specified key is mapped, or null if 
+	 * this map contains no mapping for the key
+	 */
 	public Object getData(String key) {
 		return data.get(key);
 	}
